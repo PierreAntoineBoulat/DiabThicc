84fdbecf7bb7c16bedf70a6b7ffa7fafaff26b25
NicaiseAlex <alexandre.nicaise@etu.univ-cotedazur.fr>
2019-04-18 15:27:45 +0200

    connect

--- app/src/main/java/com/game/pa2a/diabthicc/ConnectActivity.java
+++ app/src/main/java/com/game/pa2a/diabthicc/ConnectActivity.java
@@ -1,22 +1,25 @@
 package com.game.pa2a.diabthicc;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.internal.BottomNavigationItemView;
-import android.support.design.internal.BottomNavigationMenu;
-import android.support.design.internal.BottomNavigationMenuView;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ConnectActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        final Button button = findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent mIntent = new Intent(ConnectActivity.this, HomeActivity.class);
+                startActivity(mIntent);
+            }
+        });
     }
 }
