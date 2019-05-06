82b7f6b5392f04ceeb0cb786dcdc2c90b27f1a94
Charles Roger <charles.roger@etu.unice.fr>
2019-05-06 16:09:04 +0200

    ajout de la vue des repas et la possibilité d'ajouter des repas

--- app/src/main/java/com/game/pa2a/diabthicc/TodayActivity.java
+++ app/src/main/java/com/game/pa2a/diabthicc/TodayActivity.java
@@ -7,16 +7,24 @@ import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.game.pa2a.diabthicc.models.CustomDate;
 
 import java.lang.reflect.Field;
 
 public class TodayActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    CustomDate currentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +69,54 @@ public class TodayActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+
+        currentDate = new CustomDate(2019,05,9,15,30);
+
+
+        final TextView textViewDate = findViewById(R.id.textViewDate);
+        textViewDate.setText(currentDate.dayFormat());
+
+        ImageButton nextDate = findViewById(R.id.imageButtonNextDate);
+        nextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( currentDate.getDay() == 31){
+                    currentDate.setDay(1);
+                    currentDate.setMonth(currentDate.getMonth()+1);
+                }
+                else {
+                    currentDate.setDay(currentDate.getDay() + 1);
+                }
+                textViewDate.setText(currentDate.dayFormat());
+            }
+        });
+
+
+        ImageButton previousDate = findViewById(R.id.imageButtonPreviousDate) ;
+        previousDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentDate.getDay() == 1){
+                    currentDate.setDay(31);
+                    currentDate.setMonth(currentDate.getMonth()-1);
+                }
+                else{
+                    currentDate.setDay(currentDate.getDay()-1);
+                }
+                textViewDate.setText(currentDate.dayFormat());
+            }
+        });
+
+
+
+        Button addMeal = findViewById(R.id.buttonAddMeal);
+        addMeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TodayActivity.this, AddMealActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
